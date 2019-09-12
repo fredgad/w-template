755252539dbced6e674d2321b2160bf59d60f011
fredgad <31152572+fredgad@users.noreply.github.com>
2019-09-12 16:38:13 +0300

    webpack done

---
+++ src/components/Example.vue
@@ -0,0 +1,19 @@
+<template>
+    <section id="main">
+        <div class="wrapper">
+            <h1>FF</h1>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+h1 {
+    color: green;
+}
+</style>
--- src/index.js
+++ src/index.js
@@ -2,3 +2,11 @@ import './js/common'
 import './css/main.css'
 import './scss/main.scss'
 window.Vue = require('vue')
+import store from './store/index'
+
+Vue.component('Wrapper', require('./components/Example.vue').default)
+
+const app = new Vue({
+    store,
+    el: '#app'
+})
